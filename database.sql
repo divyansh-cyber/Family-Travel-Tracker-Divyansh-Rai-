07a4d8817bc7f67999077dd983a6111811be271a
divyansh-cyber <140232173+divyansh-cyber@users.noreply.github.com>
2024-04-13 12:06:31 +0530

    Update database.sql

--- database.sql
+++ database.sql
@@ -21,5 +21,74 @@ INSERT INTO countries(country_code)
 VALUES('IN');
 INSERT INTO visited_countries (user_id,country_id)
 VALUES (1,1);
+INSERT INTO countries(country_code)
+VALUES('CI');
+INSERT INTO countries(country_code)
+VALUES('CL');
+INSERT INTO countries(country_code)
+VALUES('CM');
+INSERT INTO countries(country_code)
+VALUES('CN');
+INSERT INTO countries(country_code)
+VALUES('CO');
+INSERT INTO countries(country_code)
+VALUES('CR');
+INSERT INTO countries(country_code)
+VALUES('CU');
+INSERT INTO countries(country_code)
+VALUES('CY');
+INSERT INTO countries(country_code)
+VALUES('CZ');
+INSERT INTO countries(country_code)
+VALUES('DE');
+INSERT INTO countries(country_code)
+VALUES('DJ');
+INSERT INTO countries(country_code)
+VALUES('DK');
+INSERT INTO countries(country_code)
+VALUES('DO');
+INSERT INTO countries(country_code)
+VALUES('DZ');
+INSERT INTO countries(country_code)
+VALUES('EC');
+INSERT INTO countries(country_code)
+VALUES('EE');
+INSERT INTO countries(country_code)
+VALUES('EG');
+INSERT INTO countries(country_code)
+VALUES('EH');
+INSERT INTO countries(country_code)
+VALUES('ER');
+INSERT INTO countries(country_code)
+VALUES('ES');
+INSERT INTO countries(country_code)
+VALUES('ET');
+INSERT INTO countries(country_code)
+VALUES('FK');
+INSERT INTO countries(country_code)
+VALUES('FI');
+INSERT INTO countries(country_code)
+VALUES('FJ');
+INSERT INTO countries(country_code)
+VALUES('FR');
+INSERT INTO countries(country_code)
+VALUES('GA');
+INSERT INTO countries(country_code)
+VALUES('GB');
+INSERT INTO countries(country_code)
+VALUES('GE');
+INSERT INTO countries(country_code)
+VALUES('GF');
+INSERT INTO countries(country_code)
+VALUES('GH');
+INSERT INTO countries(country_code)
+VALUES('GL');
+INSERT INTO countries(country_code)
+VALUES('GM');
+INSERT INTO countries(country_code)
+VALUES('GN');
+INSERT INTO countries(country_code)
+VALUES('GQ');
+
 
 
